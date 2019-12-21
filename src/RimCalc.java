a2f333d955147d84b49c62fda33687c5752eac95
Demotivirus <demotivirus@gmail.com>
2019-12-21 23:54:32 +0300

    Fix bugs for rim numbers

--- src/RimCalc.java
+++ src/RimCalc.java
@@ -4,6 +4,9 @@ public class RimCalc {
     private int scndOp;
     private String symbol;
     private int tmp;
+    private String math;
+    private int res1;
+    private int res2;
 
     public void calculate(String str1, String symbol, String str2) throws ArithmeticException{
         fstOp = transpose(str1);
@@ -29,16 +32,72 @@ public class RimCalc {
     }
 
     private void operation(){
-        double tmpDivision = 0.0;
+        String revSymb = "";
+        if(tmp == 0)
+            new ArithmeticException();
         switch (symbol){
             case "+":
-                System.out.println(fstOp + scndOp); break;
+                tmp = fstOp + scndOp; break;
             case "-":
-                System.out.println(fstOp - scndOp); break;
+                tmp = fstOp - scndOp;
+                if (tmp < 0){
+                    tmp *= -1;
+                    System.out.println("-" + logick());
+                    return;
+                }
+                break;
             case "*":
-                System.out.println(fstOp * scndOp); break;
-            case "/": tmpDivision +=fstOp; tmpDivision /= scndOp;
-                System.out.println(tmpDivision); break;
+                tmp = fstOp * scndOp; break;
+            case "/":
+                tmp = fstOp / scndOp; break;
+            default: throw new ArithmeticException();
+        }
+
+        if(tmp == 0)
+            throw new ArithmeticException();
+        else if (tmp > 0)
+            System.out.println(logick());
+    }
+
+    private String logick(){
+        String str = "";
+        res1 = tmp / 10;
+        res2 = tmp % 10;
+        if(tmp > 10)
+            str = arithmeticLogickI() + arithmeticLogickII();
+        else str += arithmeticLogickII();
+        return str;
+    }
+
+    private String arithmeticLogickI(){
+        switch (res1){
+            case 1: return math = "X";
+            case 2: return math = "XX";
+            case 3: return math = "XXX";
+            case 4: return math = "XL";
+            case 5: return math = "L";
+            case 6: return math = "LX";
+            case 7: return math = "LXX";
+            case 8: return math = "LXXX";
+            case 9: return math = "XC";
+            case 10: return math = "C";
+            default: throw new ArithmeticException();
+        }
+    }
+
+    private String arithmeticLogickII(){
+        switch (res2){
+            case 0: return math = "";
+            case 1: return math = "I";
+            case 2: return math = "II";
+            case 3: return math = "III";
+            case 4: return math = "IV";
+            case 5: return math = "V";
+            case 6: return math = "VI";
+            case 7: return math = "VII";
+            case 8: return math = "VIII";
+            case 9: return math = "IX";
+            case 10: return math = "X";
             default: throw new ArithmeticException();
         }
     }
